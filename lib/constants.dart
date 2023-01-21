df79e440976b14ed250b6b2685f8bba38b2b25f9
AYUSH KUMAR SINGH <ayushkumarsingh0708@gmail.com>
2023-01-21 18:30:40 +0530

    First Commit

---
+++ lib/constants.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+Color kNavColor = const Color.fromARGB(1, 255, 255, 255);
+
+Color kBtnColor = const Color.fromRGBO(100, 225, 220, 0.7);
+Color kNavIconColor = Colors.grey.shade700;
+Color kTabBarUnselectedColor = const Color.fromRGBO(181, 185, 194, 1);
+Color kTabBarSelectedColor = const Color.fromRGBO(80, 80, 80, 1);
+List<String> post = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '3',
+  '2',
+  '9',
+  '6',
+  '1',
+];
+// BookMySleep UI Constants
+
+Color kPrimaryColor = const Color.fromRGBO(69, 210, 218, 1);
+Color kTextColor = const Color.fromARGB(255, 26, 81, 120);
+Color kWhiteColor = Colors.white;
+Color kBlackColor = Colors.black;
+
+Color kGreyColor = Colors.grey;
